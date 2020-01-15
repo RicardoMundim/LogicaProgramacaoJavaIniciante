67f0a38ca2018e07180344cd3d63bd08387663f0
Ricardo <ritiny@gmail.com>
2020-01-14 23:23:29 -0300

    Exercícios do Capítulo 3

---
+++ Capitulo3/src/TiposNumericos.java
@@ -0,0 +1,18 @@
+
+public class TiposNumericos {
+
+	public static void main(String[] args) {
+		Byte numeroByte = 1; // -127 até 127 
+		
+		Short numeroShort = 1; // -32768 ate 32767
+		
+		Integer numeroInteger = 1; // -2147483648 ate 2147483647
+		
+		Long numeroLong = 1L; // -9.223.372.036.854.775.808 ate 9.223.372.036.854.775.807
+		
+		Float numeroFloat = 1.0f; 
+		
+		Double numeroDouble = 1.0;
+	}
+
+}
