6b577e9744e899c2b55337e4d7dc992cdae4fd77
Ricardo <ritiny@gmail.com>
2020-02-20 23:12:17 -0300

    Exercícios do capítulo 10

---
+++ Capitulo10/src/escritaarquivos/EscritaArquivos.java
@@ -0,0 +1,30 @@
+package escritaarquivos;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class EscritaArquivos {
+
+	public static void main(String[] args) throws IOException   { 
+		Scanner scanner = new Scanner(System.in);
+		
+		ArrayList<String> linhas = new ArrayList<String>();
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println("Jogador " + (i + 1) + ": ");
+			String nome = scanner.nextLine();
+			
+			linhas.add(nome);
+		}
+		
+		Path arquivo = Paths.get("C:\\Projetos\\LogicaProgramacaoJavaIniciante\\Capitulo10\\EscritaLeitura\\arquivo.txt"); 
+		Files.write(arquivo, linhas);
+		
+		scanner.close();
+	}
+
+}
